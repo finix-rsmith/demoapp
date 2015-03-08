f55fc32e7cc2e523d827b12f4e24d1169d275b71
finix-rsmith <rsmith@finixcreative.com>
2015-03-08 12:33:29 -0400

    App - fetch local json
    Add brand json and set $resource query params to fetch it

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var app = angular.module('app', ['angularGit']);
 
 app.controller('MainController', function($scope, AngularGit){
-	var params = {category: 'commits', sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
-	console.log(AngularGit.get(params))
+	var params = {file: 'brand'}//, sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
+	//console.log(AngularGit.get(params))
+	console.log(AngularGit.query(params))
 });
 
 angular.module('angularGit', ['ngResource'])
 	.factory('AngularGit', function($resource){
-		return $resource('https://api.github.com/repos/angular/angular.js/:category/:sha')
+		return $resource('json/:file')
 	})
