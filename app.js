51e0d7732692b56675759799a34898840323a228
finix-rsmith <rsmith@finixcreative.com>
2015-03-08 16:46:42 -0400

    Module - update var syntax
    Add semicolon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('appDemo', ['angularGit']);
 
 app.controller('MainController', function($scope, AngularGit){
-	var params = {file: 'brand'}//, sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
+	var params = {file: 'brand'};//, sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
 	//console.log(AngularGit.get(params))
 	console.log(AngularGit.query(params))
 });
