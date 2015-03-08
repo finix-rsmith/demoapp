63aa561584b162da8cd93d3058014e4db68237a7
finix-rsmith <rsmith@finixcreative.com>
2015-03-08 12:07:44 -0400

    Base Files - first commit pt2
    Add app and Index

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', ['app.angularGit']);
+
+app.controller('MainController', function($scope, AngularGit){
+	var params = {category: 'commits', sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
+	console.log(AngularGit.get(params))
+});
+
+angular.module('app', ['app.angularGit'])
+	.factory('AngularGit', function($resource){
+		return $resource('https://api.github.com/repos/angular/angular.js/:category/:sha')
+	})
