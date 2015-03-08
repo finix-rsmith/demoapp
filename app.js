8ac05c2de09a27b05467d20e8e8f462c43b7f1d7
finix-rsmith <rsmith@finixcreative.com>
2015-03-08 17:13:06 -0400

    Module - remove var
    Use regular syntax

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var app = angular.module('appDemo', ['angularGit']);
-
-app.controller('MainController', function($scope, AngularGit){
-	var params = {file: 'brand'}
-	console.log(AngularGit.query(params));
-});
+angular.module('appDemo', ['angularGit'])
+	.controller('MainController', function($scope, AngularGit){
+		var params = {file: 'brand'}
+		console.log(AngularGit.query(params));
+	});
 
 angular.module('angularGit', ['ngResource'])
 	.factory('AngularGit', function($resource){
 		return $resource('json/:file.json')
-	})
+	});
