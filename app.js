d9a80bde357f2dd60a60a804909d4ce868396c3a
finix-rsmith <rsmith@finixcreative.com>
2015-03-08 16:40:32 -0400

    Module - revert to params
    Set file to brand

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = angular.module('app', ['angularGit']);
 app.controller('MainController', function($scope, AngularGit){
 	var params = {file: 'brand'}//, sha: '80e7a4558490f7ffd33d142844b9153a5ed00e86'}
 	//console.log(AngularGit.get(params))
-	console.log(AngularGit.query())
+	console.log(AngularGit.query(params))
 });
 
 angular.module('angularGit', ['ngResource'])
 	.factory('AngularGit', function($resource){
-		return $resource('json/')
+		return $resource('json/:file.json')
 	})
