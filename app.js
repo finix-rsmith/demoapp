d874d992f732f64a31084ef16489969fc30a953a
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 17:55:46 -0400

    App - syntax
    Update syntax to close any open tags

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-angular.module('appDemo', ['app.angularGit'])
-	.controller('MainController', function($scope, AngularGit){
-		var brand = {file: 'brand.json'};
-		var web = {file: 'web.json'};
+angular.module("appDemo", ["app.angularGit"])
+	.controller("MainController", function($scope, AngularGit){
+		var brand = {file: "brand.json"};
+		var web = {file: "web.json"};
 		console.log(AngularGit.get(brand))
 		console.log(AngularGit.get(web))
 		console.log(AngularGit.query(web))
 	});
 
-angular.module('app.angularGit', ['ngResource'])
-	.factory('AngularGit', function($resource){
-		return $resource('json/:file')
+angular.module("app.angularGit", ["ngResource"])
+	.factory("AngularGit", function($resource){
+		return $resource("json/:file")
 	});
