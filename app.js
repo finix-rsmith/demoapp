5e322d007b2b6319e39cd744d782c59b4df5eb0c
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:39:58 -0400

    App - set paths to Finix server
    Set product paths to finix servers

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var params = {folder: "json"};
-		var params2 = {folder: "json", product: "brand.json"};
+		var params = {folder: "products"};
+		var params2 = {folder: "products", product: "brand"};
 		console.log(AngularGit.query(params))
 		console.log(AngularGit.get(params2))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("http://finix-rsmith.github.io/demoapp/:folder/:product")
+		return $resource("http://www.finixcreative.com/reboot/app/pages/products/:folder/:product")
 	})
