303c8d71b2268cc0a10695dac7878e1b6d978e8f
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:19:33 -0400

    Revert - return to last working model
    Use original version

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var params = {folder: "json", file: "brand"};
-		console.log(AngularGit.get(params))
+		var params = {category: "commits"};
+		console.log(AngularGit.query(params))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("http://finix-rsmith.github.io/demoapp/:folder/:file.json")
+		return $resource("https://api.github.com/repos/angular/angular.js/:category/:sha")
 	})
