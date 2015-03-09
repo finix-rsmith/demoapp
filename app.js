a50eb31008e404c268824b843fe6978ca2234d41
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:29:57 -0400

    App - use local files
    Set $resource path to local /json directory

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var params2 = {category: "commits", sha: "80e7a4558490f7ffd33d142844b9153a5ed00e86"};
-		var params = {category: "commits"};
-		console.log(AngularGit.get(params2))
+		var params = {folder: "json"};
+		var params2 = {folder: "json", product: "brand"};
 		console.log(AngularGit.query(params))
+		console.log(AngularGit.get(params2))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("https://api.github.com/repos/angular/angular.js/:category/:sha")
+		return $resource("http://finix-rsmith.github.io/demoapp/:folder/:product")
 	})
