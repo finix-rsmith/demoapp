931c2d01a9911e1583dda834f4d593ddf2301cca
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 17:17:28 -0400

    Module - syntax & log
    Fix syntax and add console.log()

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 angular.module('appDemo', ['app.angularGit'])
 	.controller('MainController', function($scope, AngularGit){
-		var params = {file: 'brand'}
-		console.log(AngularGit.query(params))
+		var params = {'file': 'brand.json'}
+		console.log(AngularGit.get(params))
+		console.log(AngularGit.query())
 	});
 
 angular.module('app.angularGit', ['ngResource'])
 	.factory('AngularGit', function($resource){
-		return $resource('json/:file.json')
+		return $resource('json/:file')
 	});
