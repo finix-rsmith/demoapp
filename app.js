2515123aebe688c0ab807aef943c588d734450f0
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:22:38 -0400

    Controller - log get
    Add get method to log individual piece

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
+		var params2 = {category: "commits", sha: "80e7a4558490f7ffd33d142844b9153a5ed00e86"};
 		var params = {category: "commits"};
+		console.log(AngularGit.get(params2))
 		console.log(AngularGit.query(params))
 	});
 
