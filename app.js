d186fd1817e4d92459501dda609e7fd775f993d6
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:12:31 -0400

    App - replace with local file
    Use syntax to create path to local files

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var params = {category: "commits"};
+		var params = {folder: "json", file: "brand"};
 		console.log(AngularGit.query(params))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("https://api.github.com/repos/angular/angular.js/:category/:sha")
+		return $resource(":folder/:file")
 	})
