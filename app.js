b18ba977e3c694263e96431ff4ae995116f7b0a2
finix-rsmith <rsmith@finixcreative.com>
2015-03-09 18:07:37 -0400

    App - use commits
    Use data set from tutorial

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var brand = {file: "brand.json"};
-		var web = {file: "web.json"};
-		console.log(AngularGit.get(brand))
-		console.log(AngularGit.get(web))
-		console.log(AngularGit.query(web))
+		var params = {category: "commits"};
+		console.log(AngularGit.query(params))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("json/:file")
-	});
+		return $resource("https://api.github.com/repos/angular/angular.js/:category/:sha")
+	})
