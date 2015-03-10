899aa6afbec22ed2cbff36e595587fcdcc44be5a
finix-rsmith <rsmith@finixcreative.com>
2015-03-10 11:17:05 -0400

    App - set params
    Set "product" name to "@id" to extract the id from each json file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 angular.module("appDemo", ["app.angularGit"])
 	.controller("MainController", function($scope, AngularGit){
-		var params = {folder: "products"};
-		var params2 = {folder: "products", product: "brand.json"};
+		var params = {folder: "json"};
+		var params2 = {folder: "json", product: "@id", format: ".json"};
 		console.log(AngularGit.query(params))
 		console.log(AngularGit.get(params2))
 	});
 
 angular.module("app.angularGit", ["ngResource"])
 	.factory("AngularGit", function($resource){
-		return $resource("http://www.finixcreative.com/reboot/app/pages/products/:folder/:product")
+		return $resource(":folder/:product:format")
 	})
